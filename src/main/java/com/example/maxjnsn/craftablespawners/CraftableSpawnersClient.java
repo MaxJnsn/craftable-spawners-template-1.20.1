0144d59af3959a5655e4bc41736db92720b12768
MaxJNSN <maxjansen7777@gmail.com>
2024-05-18 12:28:45 +0200

    Initial commit

---
+++ src/main/java/com/example/maxjnsn/craftablespawners/CraftableSpawnersClient.java
@@ -0,0 +1,10 @@
+package com.example.maxjnsn.craftablespawners;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftableSpawnersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
